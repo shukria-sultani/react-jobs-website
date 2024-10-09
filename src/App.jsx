4a83ddced80f29864185901e429616a8336f30f1
shukria-sultani <shukriasul5@gmail.com>
2024-10-09 21:52:27 +0430

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar from './Components/Navbar'
+function App() {
+    
+        
+  return (
+    <>
+       <Navbar></Navbar>
+    </>
+  )
+}
+
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+       <div>
+         <img src="" alt="logo" className='logo' />
+         <span>React Jobs</span>
+         <ul>
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Add Job</li>
+         </ul>
+       </div>
+       <hr />
+    </nav>
+  )
+}
