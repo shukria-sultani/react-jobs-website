bc80065d62e4003349ce8ad15ede26650fd64393
Shukria <shukriasul5@gmail.com>
2024-10-25 20:38:10 +0430

    Updated the navbar with bootstrap classes to make it responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
   return (
-    <nav>
-       <div>
-         <img src="" alt="logo" className='logo' />
-         <span>React Jobs</span>
-         <ul>
-            <li>Home</li>
-            <li>Jobs</li>
-            <li>Add Job</li>
-         </ul>
-       </div>
-       <hr />
+    <nav className="navbar navbar-expand-lg  navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand text-light fw-bold" style={{ fontSize: '2rem' }} href="#">
+          React Jobs
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto mt-2">
+            <li className="nav-item">
+              <a
+                className="nav-link active text-light bg-primary"
+                aria-current="page"
+                href="#"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link  text-light" href="#">
+                Jobs
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-light" href="#">
+                Add Job
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
     </nav>
-  )
+
+  );
 }
