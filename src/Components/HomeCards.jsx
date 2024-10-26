401073adda47f185579a585bb09a5ebd5bf2d1d4
Shukria <shukriasul5@gmail.com>
2024-10-26 15:26:46 +0430

    Updated the cards background color

--- src/Components/HomeCards.jsx
+++ src/Components/HomeCards.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 
 export default function HomeCards() {
+  const style1 = {
+     backgroundColor : "#e5eaf5",
+ 
+  }
+  const style2 = {
+    backgroundColor : "#c3e3f3 "
+ }
   return (
     <div className='container' >
           <div className='d-lg-flex flex-dir-col pt-5 gap-5 justify-content-center'>
-            <div className='bg-primary p-3 m-sm-4'>
+            <div  className='card p-4 m-sm-4' style={style1}>
                <h4 className='fw-bold'>For Devlopers</h4> 
                  <p className='lead'>Browse our React jobs and start your career today</p>
-                 <button className="bg-dark text-light p-2 rounded-2">Browse Jobs</button>
+                 <button className="bg-dark text-light p-2 rounded-3">Browse Jobs</button>
                  </div>
 
-                 <div className='bg-primary p-3 m-sm-4'>
+                 <div className=' card p-4 m-sm-4'  style={style2}>
                <h4 className='fw-bold ' >For Devlopers</h4> 
                  <p className='lead'>Browse our React jobs and start your career today</p>
-                 <button className="bg-dark text-light p-2 rounded-2 ">Browse Jobs</button>
+                 <button className="bg-dark text-light p-2 rounded-3 ">Browse Jobs</button>
                  </div>
           </div>
     </div>
