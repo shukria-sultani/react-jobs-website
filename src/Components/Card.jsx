a6408526a035aabf4df13180fc6641f394c1d7ee
Shukria <shukriasul5@gmail.com>
2024-10-27 21:54:40 +0430

    Updated the background color with a default background if no style is passed

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Card({children, style}) {
+export default function Card({children, style ={backgroundColor: "#fff"}}) {
   return (
     <div className={`card p-4 m-sm-4 `} style={style}>
           {children} 
