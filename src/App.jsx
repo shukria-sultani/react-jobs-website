6f65961ed1eafd02da77b41d94c3b557fce6afd6
Shukria <shukriasul5@gmail.com>
2024-10-29 21:04:50 +0430

    Created the home page and main layout routes.

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,35 @@ import Hero from './Components/Hero'
 import HomeCards from "./Components/HomeCards"
 import JobsListing from './Components/JobsListing'
 import ViewAllJobs from './Components/ViewAllJobs'
+import HomePage from './Pages/HomePage'
+import MainLayout from './Layouts/MainLayout'
+import {Route,
+    createBrowserRouter, 
+    createRoutesFromElements,
+    RouterProvider} from "react-router-dom"
+
+
+      
+  const router  = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='' element ={<MainLayout />}>
+    <Route index element= {<HomePage />}/>
+    </Route>
+  )
+  )
+
 function App() {
-    
+
         
   return (
     <>
-       <Navbar></Navbar>
+       {/* <Navbar></Navbar>
        <Hero></Hero>
        <HomeCards></HomeCards>
        <JobsListing></JobsListing>
-       <ViewAllJobs></ViewAllJobs>
+       <ViewAllJobs></ViewAllJobs> */}
+
+        <RouterProvider router = {router}/>
     </>
   )
 }
