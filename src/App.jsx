5e347cc1a8555997c726633b5be39461d1edba90
Shukria <shukriasul5@gmail.com>
2024-10-29 21:23:04 +0430

    Added the Jobs page and it's route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import { useState } from 'react'
-import Navbar from './Components/Navbar'
-import Hero from './Components/Hero'
-import HomeCards from "./Components/HomeCards"
-import JobsListing from './Components/JobsListing'
-import ViewAllJobs from './Components/ViewAllJobs'
+
 import HomePage from './Pages/HomePage'
 import MainLayout from './Layouts/MainLayout'
+import JobsPage from './Pages/JobsPage'
 import {Route,
     createBrowserRouter, 
     createRoutesFromElements,
     RouterProvider} from "react-router-dom"
 
-
-      
   const router  = createBrowserRouter(
     createRoutesFromElements(
-    <Route path='' element ={<MainLayout />}>
-    <Route index element= {<HomePage />}/>
+    <Route path='/' element ={<MainLayout />}>
+       <Route index element= {<HomePage />}/>
+       <Route path ="/jobs" element= {<JobsPage />}/>
+
     </Route>
   )
   )
