32c846167b0bdd709e355544fff9e16486b573f5
Shukria <shukriasul5@gmail.com>
2024-12-29 00:13:28 +0430

    Created the Add Job page with functionality to add a job, along with enhanced styling.

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,47 @@ import HomePage from './Pages/HomePage'
 import MainLayout from './Layouts/MainLayout'
 import JobsPage from './Pages/JobsPage'
 import JobPage, {jobLoader} from './Pages/JobPage'
+import AddJobPage from './Pages/AddJobPage'
 import NotFoundPage from './Pages/NotFoundPage'
 
 
 import {Route,
     createBrowserRouter, 
     createRoutesFromElements,
-    RouterProvider} from "react-router-dom"
+    RouterProvider,
+    } from "react-router-dom"
 
+
+function App() { 
+  const addJob = async (newJob) => {
+    const res = await fetch("/api/jobs", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newJob)
+    });
+
+    if (!res.ok) {
+        throw new Error('Failed to add job');
+    }
+
+    const data = await res.json(); 
+    return data; 
+};
   const router  = createBrowserRouter(
     createRoutesFromElements(
     <Route path='/' element ={<MainLayout />}>
        <Route index element= {<HomePage />}/>
        <Route path ="/jobs" element= {<JobsPage />}/>
        <Route path="/jobs/:id" element= {<JobPage />}  loader={jobLoader}></Route>
+       <Route path="/addjob" element= {<AddJobPage addJobSubmit={addJob}/>}></Route>
        <Route path="*" element={<NotFoundPage />}></Route>
      
     </Route>
+ 
   )
   )
-function App() {   
   return (
     <>
         <RouterProvider router = {router}/>
